2886322b25331d0f5b1174db093302bbd35c085f
Michal <killbwoy@gmail.com>
2023-05-15 21:38:45 +0200

    3rd correction

--- main.cpp
+++ main.cpp
@@ -61,6 +61,13 @@ int main() {
             cout << " >>> MENU UZYTKOWNIKA <<<" << endl;
             cout << "---------------------------" << endl;
             cout << "1. Dodaj adresata" << endl;
+            cout << "2. Wyszukaj po imieniu" << endl;
+            cout << "3. Wyszukaj po nazwisku" << endl;
+            cout << "4. Wyswietl adresatow" << endl;
+            cout << "5. Usun adresata" << endl;
+            cout << "6. Edytuj adresata" << endl;
+            cout << "---------------------------" << endl;
+            cout << "7. Zmien haslo" << endl;
             cout << "8. Wyloguj sie" << endl;
             cout << "---------------------------" << endl;
 
